f7ac0276b72775dcac4965c5d8aaa518cbd8d09b
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2018-08-05 19:29:49 -0300

    Adjust mklinks

--- mklinks.sh
+++ mklinks.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-for d in $(ls -d ../*/) ; do  ln -s $d geopsy-${d:3:-1}; done
+for d in $(ls -d ../*/) 
+do
+  dd="${d:3:-1}"
+  echo "$dd"
+  if [ "$dd" != "geopsy-cmake" ]; then
+    ln -s $d geopsy-$dd
+  fi
+done
