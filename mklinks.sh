0c90dbf894a9170f4833e092022eeea55864fe88
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2018-08-08 09:05:13 -0300

    Se opta por usar include

--- mklinks.sh
+++ mklinks.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+G_SRCS="geopsy-src" 
+mkdir $G_SRCS
 for d in $(ls -d ../*/) 
 do
   dd="${d:3:-1}"
   echo "$dd"
   if [ "$dd" != "geopsy-cmake" ]; then
-    ln -s $d geopsy-$dd
+    ln -s ../$d $G_SRCS/$dd
   fi
 done
